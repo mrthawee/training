2dd4a046e544ed08b7b6c22967e22d7f1730b257
Thawee Techathamnukool <mrthawee@gmail.com>
2020-05-05 19:47:45 -0700

    Completed BubbleSort and SelectionSort in Python

--- python/sorting/bubble.py
+++ python/sorting/bubble.py
@@ -1,11 +1,27 @@
 import random
 import sys
 import os
-A = [ 3 ,5, 7, 9, 10]
+I = [ 3 ,1, 7, 2, 10]
 
-'''
+X = [ 30, 5, 39, 4, 2, 10, 3, 9, 8, 7]
+
 def BubbleSort(A):
     i = 0
     j = 0
-'''
 
+    cnt = 0
+    for i in range(len(A)):
+        for j in range(i+1, len(A)):
+            cnt += 1
+            if (A[i] > A[j]):
+                A[i], A[j] = A[j], A[i]
+
+    print("len = %d num comparison = %d" % (len(A), cnt))
+
+print(I)
+BubbleSort(I)
+print(I)
+
+print(X)
+BubbleSort(X)
+print(X)
--- python/sorting/selection.py
+++ python/sorting/selection.py
@@ -2,6 +2,27 @@ import random
 import sys
 import os
 
-I = [ 3, 1, 9, 4, 2 ]
-
 def SelectionSort(A):
+    i = 0
+    j = 0
+
+    cnt = 0
+    for i in range(len(A)):
+        min_idx = i
+        for j in range(i+1, len(A)):
+            cnt += 1
+            if (A[min_idx] > A[j]):
+                min_idx = j 
+        A[i], A[min_idx] = A[min_idx], A[i]
+
+    print("len = %d num comparison = %d" % (len(A), cnt))
+
+I = [ 3 ,1, 7, 2, 10]
+print(I)
+SelectionSort(I)
+print(I)
+
+X = [ 30, 5, 39, 4, 2, 10, 3, 9, 8, 7]
+print(X)
+SelectionSort(X)
+print(X)
